1816bb3827847942b7b87370681e007333c206aa
Daniel Szuster <37206928+SzustyDaniel@users.noreply.github.com>
2022-01-01 17:43:59 +0200

    Added methods to service.

--- projects/angular-azure-insights/src/lib/angular-azure-insights.service.ts
+++ projects/angular-azure-insights/src/lib/angular-azure-insights.service.ts
@@ -1,7 +1,17 @@
 import { Inject, Injectable } from '@angular/core';
 import {
+  IEventTelemetry,
+  IExceptionTelemetry,
+  IMetricTelemetry,
+  IPageViewPerformanceTelemetry,
+  IPageViewTelemetry,
+  ITraceTelemetry,
+} from '@microsoft/applicationinsights-common';
+import {
   ApplicationInsights,
   IConfiguration,
+  ICustomProperties,
+  ITelemetryItem,
 } from '@microsoft/applicationinsights-web';
 
 @Injectable({
@@ -13,7 +23,68 @@ export class AngularAzureInsightsService {
   constructor(
     @Inject('APP_AZURE_CONFIGURATION') configuration: IConfiguration
   ) {
-    this.appInsights = new ApplicationInsights({ config: configuration }); // TODO init from for root definition in module.
+    this.appInsights = new ApplicationInsights({ config: configuration });
     this.appInsights.loadAppInsights();
   }
+
+  // Trigger controlled flush of the collected data by AppInsights (Not required since it's done automatically)
+  public flush(): void {
+    this.appInsights.flush();
+  }
+
+  // Modify collected telemetry using a telemetry initializer, for more information please read the documentation at https://github.com/microsoft/applicationinsights-js
+  public addTelemetryProperty(
+    telemetryItem: (item: ITelemetryItem) => boolean | void
+  ): void {
+    this.appInsights.addTelemetryInitializer(telemetryItem);
+  }
+
+  // Trace scenarios of entering or leaving a function / method
+  public trackTrace(
+    trace: ITraceTelemetry,
+    customProperties?: ICustomProperties
+  ): void {
+    try {
+      this.appInsights.trackTrace(trace, customProperties);
+    } catch (e) {}
+  }
+
+  // Log user action or certain event
+  public trackEvent(
+    event: IEventTelemetry,
+    customProperties?: ICustomProperties
+  ): void {
+    this.appInsights.trackEvent(event);
+  }
+
+  // Log exceptions
+  public trackException(
+    exception: IExceptionTelemetry,
+    customProperties?: ICustomProperties
+  ): void {
+    this.appInsights.trackException(exception, customProperties);
+  }
+
+  // Log numeric value corresponding to a tracked performance event
+  public trackMetric(
+    metric: IMetricTelemetry,
+    customProperties?: ICustomProperties
+  ): void {
+    this.appInsights.trackMetric(metric, customProperties);
+  }
+
+  // Log a page or container that was shown to the user
+  public trackPageView(page?: IPageViewTelemetry): void {
+    this.appInsights.trackPageView(page);
+  }
+
+  public trackPagePerformance(
+    pagePerformance: IPageViewPerformanceTelemetry
+  ): void {
+    this.appInsights.trackPageViewPerformance(pagePerformance);
+  }
+
+  // TODO add logging support through outer service
+  // TODO add documentation
+  // TODO expose cookie support
 }
